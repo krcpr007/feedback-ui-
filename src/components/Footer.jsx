c810b31d987eca68298d0da97b90bb84137fc65a
Rajan kumar <krcpr007@gmail.com>
2021-12-18 21:09:31 +0530

    footer not styled

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <h3>&copy; 2021 : Rajan Kumar</h3>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
