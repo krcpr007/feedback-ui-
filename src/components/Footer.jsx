6eafe101550bb231ae4bd71c8c3ef1e5a41a4b1d
Rajan kumar <krcpr007@gmail.com>
2022-06-21 13:07:04 +0530

    current year now shows

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Footer() {
+  const date = new Date(); 
+  const year  = date.getFullYear(); 
   return (
     <footer>
       <div className="container">
-        <h3>&copy; 2021 : Rajan Kumar</h3>
+        <h3>&copy; {year} : Rajan Kumar</h3>
       </div>
     </footer>
   );
