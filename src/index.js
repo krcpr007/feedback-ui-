14865571ba8b90139630dbf2d25041f8c60bc477
Rajan kumar <krcpr007@gmail.com>
2022-06-21 13:21:08 +0530

    context added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { FeedbackProvider } from './context/FeedbackContext'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FeedbackProvider>
+      <App />
+    </FeedbackProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
